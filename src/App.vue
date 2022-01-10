db11e4a010c873c0ae46d83f341f170f5d00bdad
Thet Tun Kyaw <thettunkyaw301098@gmail.com>
2022-01-10 12:41:01 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+  <course-goal>
+    <template #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <!-- <h2>{{ slotProps["item"] }}</h2> -->
+    </template>
+  </course-goal>
+</template>
+
+<script>
+import CourseGoal from './components/CourseGoal.vue';
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+
+export default {
+  components: {
+    "the-header": TheHeader,
+    "user-info": UserInfo,
+    CourseGoal,
+    // TheHeader: TheHeader,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
