ecb5c70d7dc00e5a399eae538ab3639856b7fe20
Thet Tun Kyaw <thettunkyaw301098@gmail.com>
2022-01-13 12:04:31 +0630

    Finished component and keep-alive.

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,46 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
+    <!-- <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    ></user-info> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+    <!-- <active-goals v-show="selectedComponent==='active-goals'"></active-goals>
+    <manage-goals v-show="selectedComponent==='manage-goals'"></manage-goals> -->
   </div>
-  <course-goal>
+  <!-- <course-goal>
     <template #default="slotProps">
       <h2>{{ slotProps.item }}</h2>
-      <!-- <h2>{{ slotProps["item"] }}</h2> -->
     </template>
-  </course-goal>
+  </course-goal> -->
 </template>
 
 <script>
-import CourseGoal from './components/CourseGoal.vue';
+// import CourseGoal from './components/CourseGoal.vue';
 import TheHeader from "./components/TheHeader.vue";
-import UserInfo from "./components/UserInfo.vue";
+// import UserInfo from "./components/UserInfo.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
     "the-header": TheHeader,
-    "user-info": UserInfo,
-    CourseGoal,
+    // "user-info": UserInfo,
+    // "course-goal": CourseGoal,
+    "active-goals": ActiveGoals,
+    "manage-goals": ManageGoals,
     // TheHeader: TheHeader,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -37,6 +48,11 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
