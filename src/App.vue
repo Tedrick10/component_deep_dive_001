906ee0ace23945e479fce7aed18c770aa5ecec66
Thet Tun Kyaw <thettunkyaw301098@gmail.com>
2022-01-22 00:12:29 +0630

    Finished the project.

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,24 @@
 <template>
   <div>
     <the-header></the-header>
-    <!-- <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info> -->
     <button @click="setSelectedComponent('active-goals')">Active Goals</button>
     <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
     <keep-alive>
       <component :is="selectedComponent"></component>
     </keep-alive>
-    <!-- <active-goals v-show="selectedComponent==='active-goals'"></active-goals>
-    <manage-goals v-show="selectedComponent==='manage-goals'"></manage-goals> -->
   </div>
-  <!-- <course-goal>
-    <template #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-    </template>
-  </course-goal> -->
 </template>
 
 <script>
-// import CourseGoal from './components/CourseGoal.vue';
-import TheHeader from "./components/TheHeader.vue";
-// import UserInfo from "./components/UserInfo.vue";
+import TheHeader from "./components/layout/TheHeader.vue";
 import ActiveGoals from "./components/ActiveGoals.vue";
 import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
     "the-header": TheHeader,
-    // "user-info": UserInfo,
-    // "course-goal": CourseGoal,
     "active-goals": ActiveGoals,
     "manage-goals": ManageGoals,
-    // TheHeader: TheHeader,
   },
   data() {
     return {
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 
 import App from "./App.vue";
-import BaseBadge from "./components/BaseBadge.vue";
+import BaseBadge from "./components/UI/BaseBadge.vue";
 import BadgeList from "./components/BadgeList.vue";
-import BaseCard from "./components/BaseCard.vue";
+import BaseCard from "./components/UI/BaseCard.vue";
 import CourseGoal from "./components/CourseGoal.vue";
 
 const app = createApp(App);
@@ -12,7 +12,5 @@ app.component("base-badge", BaseBadge);
 app.component("badge-list", BadgeList);
 app.component("base-card", BaseCard);
 app.component("course-goal", CourseGoal);
-// app.component("active-goals", ActiveGoals);
-// app.component("manage-goals", ManageGoals);
 
 app.mount("#app");
